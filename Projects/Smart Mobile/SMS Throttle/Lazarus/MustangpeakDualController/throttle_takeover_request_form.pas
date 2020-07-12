50668103c2a462d3b3e3cdfca6ede1f0e8584f94
Jim Kueneman <>
2020-07-11 19:25:36 -0700

    Added the dialog to ask if a throttle can steal

---
+++ Projects/Smart Mobile/SMS Throttle/Lazarus/MustangpeakDualController/throttle_takeover_request_form.pas
@@ -0,0 +1,32 @@
+unit throttle_takeover_request_form;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
+
+type
+
+  { TFormThrottleTakeover }
+
+  TFormThrottleTakeover = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    Label1: TLabel;
+  private
+
+  public
+
+  end;
+
+var
+  FormThrottleTakeover: TFormThrottleTakeover;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
