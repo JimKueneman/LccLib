bbaacfc30d61ad8b05ed92b9ffb61fad82db7650
Jim Kueneman <JimKueneman@yahoo.com>
2020-12-17 20:04:13 -0700

    Added WebSocket to Ethernet Server

--- Projects/Smart Mobile/SMS Throttle/Target-PC/HardwareConnection/lcc_ethernet_server.pas
+++ Projects/Smart Mobile/SMS Throttle/Target-PC/HardwareConnection/lcc_ethernet_server.pas
@@ -34,6 +34,8 @@ uses
   {$ELSE}
   blcksock,
   synsock,
+  Base64,
+  sha1,
   {$ENDIF}
   lcc_threaded_circulararray,
   lcc_threaded_stringlist,
@@ -76,6 +78,8 @@ type
       procedure DoReceiveMessage;
       procedure DoSendMessage(AMessage: TLccMessage);
       procedure Execute; override;
+      procedure HandleErrorAndDisconnect;
+      procedure HandleSendConnectionNotification(NewConnectionState: TConnectionState);
       procedure SendMessage(AMessage: TLccMessage);
 
       property EthernetRec: TLccEthernetRec read FEthernetRec write FEthernetRec;
@@ -94,6 +98,23 @@ type
     public
       constructor Create(CreateSuspended: Boolean; AnOwner: TLccEthernetServer; const AnEthernetRec: TLccEthernetRec); reintroduce;
       destructor Destroy; override;
+  end;
+
+  { TLccWebSocketServerThread }
+
+  TLccWebSocketServerThread =  class(TLccEthernetServerThread)
+  private
+    FWebSocketInitialized: Boolean;
+  protected
+    procedure BuildAndSendInitializeSuccessReply(ASocket: TTCPBlockSocket; SecWebSocketKey: string);
+    procedure BuildAndSendInitializeFailureReply(ASocket: TTCPBlockSocket);
+    procedure Execute; override;
+    procedure ExtractInitializationRequest(ASocket: TTCPBlockSocket; var Method, URL, Protocol: string);
+    function ExtractInitializationHeaderKeys(ASocket: TTCPBlockSocket): TStringList;
+    function ReceiveWebSocketStateMachine(ASocket: TTCPBlockSocket): TDynamicByteArray;
+    procedure SendWebSocketStateMachine(ASocket: TTCPBlockSocket; OutStr: string);
+
+    property WebSocketInitialized: Boolean read FWebSocketInitialized write FWebSocketInitialized;
   end;
 
   { TLccEthernetThreadList }
@@ -225,6 +246,19 @@ procedure Register;
 
 implementation
 
+const
+ WEBSOCKET_OPCODE_CONTINUE = $00;
+ WEBSOCKET_OPCODE_TEXT     = $01;
+ WEBSOCKET_OPCODE_BINARY   = $02;
+ WEBSOCKET_OPCODE_CLOSE    = $08;
+ WEBSOCKET_OPCODE_PING     = $09;
+ WEBSOCKET_OPCODE_PONG     = $0A;
+
+ WEBSOCKET_CLOSE_HEADER_SERVER_LEN = 2;
+ WEBSOCKET_CLOSE_HEADER_SERVER: array[0..WEBSOCKET_CLOSE_HEADER_SERVER_LEN-1] of Byte = ($88, $80);
+ WEBSOCKET_CLOSE_HEADER_CLIENT_LEN = 6;
+ WEBSOCKET_CLOSE_HEADER_CLIENT: array[0..WEBSOCKET_CLOSE_HEADER_CLIENT_LEN-1] of Byte = ($88, $00, $56, $78, $AB, $55);
+
 procedure Register;
 begin
   {$IFNDEF FPC_CONSOLE_APP}
@@ -233,6 +267,362 @@ begin
   {$ENDIF}
   RegisterComponents('LCC',[TLccEthernetServer]);
   {$ENDIF}
+end;
+
+{ TLccWebSocketServerThread }
+
+procedure TLccWebSocketServerThread.BuildAndSendInitializeSuccessReply(ASocket: TTCPBlockSocket; SecWebSocketKey: string);
+var
+  Hash: TSHA1Digest;
+  StringStream: TStringStream;
+  Base64Stream: TBase64EncodingStream;
+  tempS: string;
+begin
+  StringStream := TStringStream.Create('');
+  Base64Stream := TBase64EncodingStream.Create(StringStream);
+
+  ASocket.SendString('HTTP/1.1 101 Switching Protocols' + CRLF);
+  ASocket.SendString('Upgrade: websocket' + CRLF);
+  ASocket.SendString('Connection: Upgrade' + CRLF);
+  tempS := SecWebSocketKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+  Hash := SHA1String(tempS);
+  tempS := SHA1Print(Hash);   // Test only
+
+  Base64Stream.Write(Hash, SizeOf(TSHA1Digest));
+  Base64Stream.Flush;  // not multiple of 3 so need to pad
+  tempS := StringStream.DataString;
+
+  ASocket.SendString('Sec-WebSocket-Accept: ' + tempS + CRLF);
+  ASocket.SendString('Sec-WebSocket-Protocol: openlcb.websocket' + CRLF);
+  ASocket.SendString('' + CRLF);
+
+  FreeAndNil(StringStream);
+  FreeAndNil(Base64Stream);
+
+end;
+
+procedure TLccWebSocketServerThread.BuildAndSendInitializeFailureReply(ASocket: TTCPBlockSocket);
+begin
+
+end;
+
+procedure TLccWebSocketServerThread.Execute;
+var
+  TxStr: String;
+  RcvByte: Byte;
+  GridConnectStrPtr: PGridConnectString;
+  GridConnectHelper: TGridConnectHelper;
+  TxList: TStringList;
+  DynamicByteArray: TDynamicByteArray;
+  LocalSleepCount, i: Integer;
+  HeaderStrings: TStringList;
+  RequestMethod, RequestURL, RequestProtocol: string;
+begin
+  FRunning := True;
+
+  HandleSendConnectionNotification(ccsListenerClientConnecting);
+  GridConnectHelper := TGridConnectHelper.Create;
+  Socket := TTCPBlockSocket.Create;          // Created in context of the thread
+  Socket.Family := SF_IP4;                  // IP4
+  Socket.ConvertLineEnd := True;            // Use #10, #13, or both to be a "string"
+  Socket.HeartbeatRate := EthernetRec.HeartbeatRate;
+  Socket.SetTimeout(0);
+  Socket.Socket := SocketHandleForListener;    // Read back the handle
+  if Socket.LastError <> 0 then
+  begin
+    HandleErrorAndDisconnect;
+    Socket.CloseSocket;
+    Socket.Free;
+    Socket := nil;
+    GridConnectHelper.Free;
+    FRunning := False
+  end else
+  begin
+    FEthernetRec.ClientIP := Socket.GetRemoteSinIP;
+    FEthernetRec.ClientPort := Socket.GetRemoteSinPort;
+    FEthernetRec.ListenerIP := Socket.GetLocalSinIP;
+    FEthernetRec.ListenerPort := Socket.GetLocalSinPort;
+    if Socket.LastError <> 0 then
+    begin
+      HandleErrorAndDisconnect;
+      Socket.CloseSocket;
+      Socket.Free;
+      Socket := nil;
+      GridConnectHelper.Free;
+      FRunning := False
+    end else
+    begin
+      HandleSendConnectionNotification(ccsListenerClientConnected);
+      try
+        try
+          LocalSleepCount := 0;
+          while not IsTerminated and (FEthernetRec.ConnectionState = ccsListenerClientConnected) do
+          begin
+
+            if not WebSocketInitialized then
+            begin
+              ExtractInitializationRequest(Socket, RequestMethod, RequestURL, RequestProtocol);
+              HeaderStrings := ExtractInitializationHeaderKeys(Socket);
+
+              if (RequestURL = '/') and (HeaderStrings.Values['Upgrade:'] = 'websocket') and (HeaderStrings.Values['Sec-WebSocket-Protocol:'] = 'openlcb.websocket') then
+                BuildAndSendInitializeSuccessReply(Socket, HeaderStrings.Values['Sec-WebSocket-Key:'])
+              else begin
+                BuildAndSendInitializeFailureReply(Socket);
+                HandleErrorAndDisconnect;
+              end;
+
+              FreeAndNil(HeaderStrings);
+              WebSocketInitialized := True;
+            end;
+
+            if not Terminated then
+            begin
+              // Handle the Socket using GridConnect
+              if Gridconnect then
+              begin
+                if LocalSleepCount >= SleepCount then
+                begin
+                  TxStr := '';
+                  TxList := OutgoingGridConnect.LockList;
+                  try
+                    if TxList.Count > 0 then
+                    begin
+                      TxStr := TxList[0];
+                      TxList.Delete(0);
+                    end;
+                  finally
+                    OutgoingGridConnect.UnlockList;
+                  end;
+
+                  if (TxStr <> '') and WebSocketInitialized then
+                    SendWebSocketStateMachine(Socket, TxStr);
+                  LocalSleepCount := 0;
+                end;
+                Inc(LocalSleepCount);
+
+                if not Terminated then
+                begin
+                  DynamicByteArray := ReceiveWebSocketStateMachine(Socket);
+                  if Length(DynamicByteArray) > 0 then
+                  begin
+                    GridConnectStrPtr := nil;
+                    for i := 0 to Length(DynamicByteArray) - 1 do
+                    begin
+                      if GridConnectHelper.GridConnect_DecodeMachine(DynamicByteArray[i], GridConnectStrPtr) and WebSocketInitialized then
+                      begin
+                        FEthernetRec.MessageStr := GridConnectBufferToString(GridConnectStrPtr^);
+                        FEthernetRec.LccMessage.LoadByGridConnectStr(FEthernetRec.MessageStr);
+                        Synchronize({$IFDEF FPC}@{$ENDIF}DoReceiveMessage);
+                      end;
+                    end;
+                  end
+                end;
+              end else
+              begin    // Handle the Socket with LCC TCP Protocol
+              {
+                if LocalSleepCount >= SleepCount then
+                begin
+                  DynamicByteArray := nil;
+                  OutgoingCircularArray.LockArray;
+                  try
+                    if OutgoingCircularArray.Count > 0 then
+                      OutgoingCircularArray.PullArray(DynamicByteArray);
+                  finally
+                    OutgoingCircularArray.UnLockArray;
+                  end;
+
+                  if Length(DynamicByteArray) > 0 then
+                  begin
+                    Socket.SendBuffer(@DynamicByteArray[0], Length(DynamicByteArray));
+                    if Socket.LastError <> 0 then
+                      HandleErrorAndDisconnect;
+                    DynamicByteArray := nil;
+                  end;
+                  LocalSleepCount := 0;
+                end;
+                Inc(LocalSleepCount);
+
+                RcvByte := Socket.RecvByte(1);
+                case Socket.LastError of
+                  0 :
+                    begin
+                      DynamicByteArray := nil;
+                      if TcpDecodeStateMachine.OPStackcoreTcp_DecodeMachine(RcvByte, FEthernetRec.MessageArray) then
+                        Synchronize({$IFDEF FPC}@{$ENDIF}DoReceiveMessage);
+                    end;
+                  WSAETIMEDOUT :
+                    begin
+
+                    end;
+                  WSAECONNRESET   :
+                    begin
+                      FEthernetRec.MessageStr := Socket.LastErrorDesc;
+                      Socket.ResetLastError;
+                      Synchronize({$IFDEF FPC}@{$ENDIF}DoClientDisconnect);
+                      FEthernetRec.MessageStr := '';
+                      Terminate;
+                    end
+                else
+                  HandleErrorAndDisconnect
+                end;    }
+              end
+            end;
+          end;
+        finally
+          HandleSendConnectionNotification(ccsListenerClientDisconnecting);
+          Socket.CloseSocket;
+          Socket.Free;
+          Socket := nil;
+          GridConnectHelper.Free;
+        end;
+      finally
+        HandleSendConnectionNotification(ccsListenerClientDisconnected);
+        Owner.EthernetThreads.Remove(Self);
+        FRunning := False;
+      end;
+    end;
+  end;
+end;
+
+procedure TLccWebSocketServerThread.ExtractInitializationRequest(ASocket: TTCPBlockSocket; var Method, URL, Protocol: string);
+var
+  s: String;
+  StrList: TSTringList;
+begin
+  s := ASocket.RecvString(120000);
+
+  s := StringReplace(s, ' ', #10, [rfReplaceAll]);
+  StrList := TStringList.Create;
+  StrList.Delimiter := #10;
+  StrList.DelimitedText := s;
+  if StrList.Count = 3 then
+  begin
+    Method := StrList[0];
+    URL := StrList[1];
+    Protocol := StrList[2];
+  end else
+  begin
+    Method := '';
+    URL := '';
+    Protocol := ''
+  end;
+end;
+
+function TLccWebSocketServerThread.ExtractInitializationHeaderKeys(ASocket: TTCPBlockSocket): TStringList;
+var
+  s: string;
+begin
+  Result := TStringList.Create;
+  repeat
+    s := ASocket.RecvString(120000);
+    s := StringReplace(s, ' ', '=', []);
+    Result.Add(s);
+  until s = '';
+end;
+
+function TLccWebSocketServerThread.ReceiveWebSocketStateMachine(ASocket: TTCPBlockSocket): TDynamicByteArray;
+var
+  AByte: Byte;
+  iHeader: Integer;
+  PayloadSize, iPayload: QWord;
+  HasMask, IsText, IsBinary, IsClose, IsContinuation, IsPing, IsPong: Boolean;
+  Mask: array[0..3] of Byte;
+begin
+  Result := nil;
+  AByte := ASocket.RecvByte(0);
+  case ASocket.LastError of
+    0 :
+      begin
+        // We assume we are in sync and this is the start of the Message
+        IsBinary := AByte and $0F = WEBSOCKET_OPCODE_BINARY;
+        IsText := AByte and $0F = WEBSOCKET_OPCODE_TEXT;
+        IsClose := AByte and $0F = WEBSOCKET_OPCODE_CLOSE;
+        IsContinuation := AByte and $0F = WEBSOCKET_OPCODE_CONTINUE;
+        IsPing := AByte and $0F = WEBSOCKET_OPCODE_PING;
+        IsPong := AByte and $0F = WEBSOCKET_OPCODE_PONG;
+
+        if (AByte and $80 = $80) then  // Last Frame of a concatinated message
+        begin
+          AByte := ASocket.RecvByte(0);
+          PayloadSize := AByte and $7F;
+          HasMask := AByte and $80 = $80;
+          if PayloadSize = 126 then
+          begin
+            PayloadSize := ASocket.RecvByte(0) shl 8;
+            PayloadSize := PayloadSize or ASocket.RecvByte(0);
+          end else
+          if PayloadSize = 127 then
+          begin
+            PayloadSize := ASocket.RecvByte(0) shl 56;
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 48);
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 40);
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 32);
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 24);
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 16);
+            PayloadSize := PayloadSize or (ASocket.RecvByte(0) shl 8);
+            PayloadSize := PayloadSize or ASocket.RecvByte(0);
+          end;
+          if HasMask then
+          begin
+            Mask[0] := ASocket.RecvByte(0);
+            Mask[1] := ASocket.RecvByte(0);
+            Mask[2] := ASocket.RecvByte(0);
+            Mask[3] := ASocket.RecvByte(0);
+          end;
+          iPayload := 0;
+          SetLength(Result, PayloadSize);
+          while iPayload < PayloadSize do
+          begin
+            AByte := ASocket.RecvByte(0);
+            Result[iPayload] := AByte XOR Mask[iPayload mod 4];
+            Inc(iPayload);
+          end;
+        end;
+        if IsClose then
+        begin
+          ASocket.ResetLastError;
+          for iHeader := 0 to WEBSOCKET_CLOSE_HEADER_SERVER_LEN - 1 do
+            ASocket.SendByte(WEBSOCKET_CLOSE_HEADER_SERVER[iHeader]);
+          HandleErrorAndDisconnect
+        end else
+        if IsPing then
+        begin
+
+        end else
+        if IsPong then
+        begin
+
+        end
+      end;
+    WSAETIMEDOUT :
+      begin
+
+      end;
+    WSAECONNRESET   :
+      begin
+        ASocket.ResetLastError;
+        HandleErrorAndDisconnect
+      end
+  end;
+end;
+
+procedure TLccWebSocketServerThread.SendWebSocketStateMachine(ASocket: TTCPBlockSocket; OutStr: string);
+var
+  i: Integer;
+begin
+  // No mask from Server to Client
+  ASocket.SendByte($80 or WEBSOCKET_OPCODE_TEXT);
+  ASocket.SendByte(Length(OutStr));
+  {$IFDEF LCC_MOBILE}
+   for i := 0 to Length(OutStr) - 1 do
+    ASocket.SendByte(Ord(OutStr[i]));
+  {$ELSE}
+  for i := 1 to Length(OutStr) do
+    ASocket.SendByte(Ord(OutStr[i]));
+  {$ENDIF}
+
+ if ASocket.LastError <> 0 then
+   HandleErrorAndDisconnect;
 end;
 
 { TLccEthernetListener }
@@ -674,25 +1064,6 @@ begin
 end;
 {$ELSE}
 procedure TLccEthernetServerThread.Execute;
-
-  procedure SendConnectionNotification(NewConnectionState: TConnectionState);
-  begin
-    FEthernetRec.ConnectionState := NewConnectionState;
-    if not FEthernetRec.SuppressNotification then
-      Synchronize({$IFDEF FPC}@{$ENDIF}DoConnectionState);
-  end;
-
-  procedure HandleErrorAndDisconnect;
-  begin
-    Owner.EthernetThreads.Remove(Self);
-    FEthernetRec.ErrorCode := Socket.LastError;
-    FEthernetRec.MessageStr := Socket.LastErrorDesc;
-    if not FEthernetRec.SuppressNotification then
-      Synchronize({$IFDEF FPC}@{$ENDIF}DoErrorMessage);
-    SendConnectionNotification(ccsListenerClientDisconnected);
-    Terminate;
-  end;
-
 var
   TxStr: String;
   RcvByte: Byte;
@@ -704,7 +1075,7 @@ var
 begin
   FRunning := True;
 
-  SendConnectionNotification(ccsListenerClientConnecting);
+  HandleSendConnectionNotification(ccsListenerClientConnecting);
   GridConnectHelper := TGridConnectHelper.Create;
   Socket := TTCPBlockSocket.Create;          // Created in context of the thread
   Socket.Family := SF_IP4;                  // IP4
@@ -736,7 +1107,7 @@ begin
       FRunning := False
     end else
     begin
-      SendConnectionNotification(ccsListenerClientConnected);
+      HandleSendConnectionNotification(ccsListenerClientConnected);
       try
         try
           LocalSleepCount := 0;
@@ -878,20 +1249,39 @@ begin
             end;
           end;
         finally
-          SendConnectionNotification(ccsListenerClientDisconnecting);
+          HandleSendConnectionNotification(ccsListenerClientDisconnecting);
           Socket.CloseSocket;
           Socket.Free;
           Socket := nil;
           GridConnectHelper.Free;
         end;
       finally
-        SendConnectionNotification(ccsListenerClientDisconnected);
+        HandleSendConnectionNotification(ccsListenerClientDisconnected);
         Owner.EthernetThreads.Remove(Self);
         FRunning := False;
       end;
     end;
   end;
 end;
+
+procedure TLccEthernetServerThread.HandleErrorAndDisconnect;
+begin
+  Owner.EthernetThreads.Remove(Self);
+  FEthernetRec.ErrorCode := Socket.LastError;
+  FEthernetRec.MessageStr := Socket.LastErrorDesc;
+  if not FEthernetRec.SuppressNotification and (FEthernetRec.ErrorCode <> 0) then
+    Synchronize({$IFDEF FPC}@{$ENDIF}DoErrorMessage);
+  HandleSendConnectionNotification(ccsListenerClientDisconnected);
+  Terminate;
+end;
+
+procedure TLccEthernetServerThread.HandleSendConnectionNotification(NewConnectionState: TConnectionState);
+begin
+   FEthernetRec.ConnectionState := NewConnectionState;
+  if not FEthernetRec.SuppressNotification then
+    Synchronize({$IFDEF FPC}@{$ENDIF}DoConnectionState);
+end;
+
 {$ENDIF}
 
 procedure TLccEthernetServerThread.SendMessage(AMessage: TLccMessage);
